9dbd236e60e8e2685e24a7694da79c9bdca59bec
Peter Balling <peter@knowmio.com>
2016-10-04 11:02:19 +0200

    So close!

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -12,7 +12,8 @@ import { Hero } from './hero';
 })
 
 export class HeroDetailComponent implements OnInit {
-	@Input() hero: Hero;
+	//@Input()
+	hero: Hero;
 
 	constructor(
 		private heroService: HeroService,
